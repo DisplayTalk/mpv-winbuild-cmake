6e7d8c747173e14bf51562fc20690ca1db0b252e
DisplayTalk <123604469+DisplayTalk@users.noreply.github.com>
2024-12-12 01:33:40 -0700

    update libplacebo to my fork

--- packages/libplacebo.cmake
+++ packages/libplacebo.cmake
@@ -10,9 +10,9 @@ ExternalProject_Add(libplacebo
         fast_float
         libdovi
         xxhash
-    GIT_REPOSITORY https://code.videolan.org/haasn/libplacebo.git
+    GIT_REPOSITORY https://github.com/DisplayTalk/libplacebo.git
     SOURCE_DIR ${SOURCE_LOCATION}
-    GIT_CLONE_FLAGS "-b fix-desat"
+    GIT_CLONE_FLAGS "--filter=tree:0"
     GIT_SUBMODULES ""
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ""
